ce0772761a458bfe7443d046ade17f2bab9e96ad
Juan Camilo <juancami98.11@hotmail.com>
2019-12-12 17:53:38 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb+srv://catomas:trucha1998@cluster0-5uco4.mongodb.net/Cafe'
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
